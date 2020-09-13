a4feab42a377e3e3f71bb2a65e1fb33838e886cd
OpenSoftsrl <44416921+OpenSoftsrl@users.noreply.github.com>
2020-09-12 23:59:18 -0400

    Add project files.

---
+++ TareaSemana1/TareaSemana1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TareaSemana1.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TareaSemana1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new StartPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
